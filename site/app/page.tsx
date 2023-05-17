adef352c3dca1c998e3bf588a77e6406f505764e
James Macharia <jamesmasher13@gmail.com>
2023-05-18 01:26:32 +0300

    Fix sites eslint errors

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,13 +1,15 @@
+import * as React from 'react';
+
 const Home = () => {
   return (
     <main
       style={{
-        height: "100vh",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: "5rem",
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: '5rem',
       }}
     >
       <h1>This is the Home page to the personal site of mine!</h1>
